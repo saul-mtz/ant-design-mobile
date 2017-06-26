cd55a04c30f7397901f101342385ea0e9738b986
Saúl Martínez <saul.mtz.v@gmail.com>
2017-06-26 17:46:03 -0500

    Spanish Locale

---
+++ components/locale-provider/es_ES.tsx
@@ -0,0 +1,13 @@
+import moment from 'moment';
+moment.locale('es');
+
+import Pagination from '../pagination/locale/es_ES';
+import DatePicker from '../date-picker/locale/es_ES';
+import InputItem from '../input-item/locale/es_ES';
+
+export default {
+  locale: 'es',
+  Pagination,
+  DatePicker,
+  InputItem,
+};
